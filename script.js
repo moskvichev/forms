dc8ff054266eefc83cdff4f2d68bc06c4401a14f
moskvichev evgeniy <moskvichev_e@bk.ru>
2024-04-02 00:14:53 +0400

    window.onload

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 
 console.log('script file added');
 
-const btnSubmit = document.querySelector('.form_submit');
-const firstName = document.querySelector('.first_name');
-const lastName = document.querySelector('.last_name');
-const email = document.querySelector('.email_field');
-const website = document.querySelector('.website_field');
-const textField = document.querySelector('.textarea_field');
-const textOutput = document.querySelector('.text-output');
+var btnSubmit = document.querySelector('.form_submit');
+var firstName = document.querySelector('.first_name');
+var lastName = document.querySelector('.last_name');
+var email = document.querySelector('.email_field');
+var website = document.querySelector('.website_field');
+var textField = document.querySelector('.textarea_field');
+var textOutput = document.querySelector('.text-output');
 
-const submitFunc = () => {
+var submitFunc = () => {
   let sub = [firstName.value, lastName.value, email.value, website.value, textField.value];
   for (item of sub) {
     if (item == null || item == '') {
@@ -20,30 +20,30 @@ const submitFunc = () => {
       textOutput.innerHTML = `${sub[0]}<br/>${sub[1]}<br/>${sub[2]}<br/>${sub[3]}<br/>${sub[4]}<br/>`;
     }
   }
-
   save(firstName.value, lastName.value, email.value, website.value, textField.value);
 
   function save(firstName, lastName, email, website, textField) {
     if (window.localStorage) {
-      localStorage.loan_firstName = firstName;
-      localStorage.loan_lastName = lastName;
-      localStorage.loan_email = email;
-      localStorage.loan_website = website;
-      localStorage.loan_textField = textField;
-      console.log('datas added');
+      localStorage.form_firstName = firstName;
+      localStorage.form_lastName = lastName;
+      localStorage.form_email = email;
+      localStorage.form_website = website;
+      localStorage.form_textField = textField;
+      console.log(localStorage.form_firstName);
     }
   }
 
   // восстанавливать заполненные поля
-
-  window.onload = function () {
-    if (window.localStorage && localStorage.loan_firstName) {
-      document.querySelector('.first_name').value = localStorage.loan_firstName;
-      document.querySelector('.last_name').value = localStorage.loan_lastName;
-      document.querySelector('.email_field').value = localStorage.loan_email;
-      document.querySelector('.website_field').value = localStorage.loan_website;
-      document.querySelector('.textarea_field').value = localStorage.loan_textField;
-    }
-  };
 };
+
+window.onload = function () {
+  if (window.localStorage && localStorage.form_firstName) {
+    document.querySelector('.first_name').value = localStorage.form_firstName;
+    document.querySelector('.last_name').value = localStorage.form_lastName;
+    document.querySelector('.email_field').value = localStorage.form_email;
+    document.querySelector('.website_field').value = localStorage.form_website;
+    document.querySelector('.textarea_field').value = localStorage.form_textField;
+  }
+};
+
 btnSubmit.onclick = submitFunc;
